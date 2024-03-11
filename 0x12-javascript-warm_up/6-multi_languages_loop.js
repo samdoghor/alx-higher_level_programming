901df785536ae08f159f5b6ab42520e23dde7b29
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2024-03-11 16:19:44 +0100

    0x12-javascript-warm_up - 2

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const words = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let x;
 for (x in words) {
-	console.log(words[x]);
+  console.log(words[x]);
 }
